c70498ef28fa10b9499274a6115db6025c23ee08
tracey <tracey.d.huang@gmail.com>
2022-06-14 13:52:24 +1000

    Remove nested for loop

--- script.js
+++ script.js
@@ -2,17 +2,13 @@
 //Select container element from HTML file 
 const container = document.querySelector('.container');
 //use for loop to repeat 16 times 
-for (let i = 0; i < 16; i ++) {
-    //create column with 16 divs 
+for (let i = 0; i < 256; i ++) {
+    //create column 
     //create element in memory for the div 
-    const gridCol = document.createElement('div');
+    const grid = document.createElement('div');
     //Append newly created grid element to the container div in HTML file 
-    container.append(gridCol);
-    gridCol.textContent = i + 1;
-        //create row with 16 divs 
-        for (let j = 0; j < 15; j ++) {
-            const gridRow = document.createElement('div');
-            container.append(gridRow);
-            gridRow.textContent = j + 1;
-        }
+    container.append(grid;
+
 }
+
+//Grid changes color when mouse passes over it
