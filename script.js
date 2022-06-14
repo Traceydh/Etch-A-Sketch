c892c439b6976f4bfe20cc4c18aca22195cf2830
tracey <tracey.d.huang@gmail.com>
2022-06-14 12:36:49 +1000

    Create 16 div elements

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //create a webpage with 16x16 grid of square divs 
 //Select container element from HTML file 
 const container = document.querySelector('.container');
-//create element in memory for the div 
-const grid = document.createElement('div');
-//Append newly created grid element to the container div in HTML file 
-container.append(grid);
-//use for loop to repeat above step 
+//use for loop to repeat 16 times 
+for (let i = 0; i < 16; i ++) {
+    //create element in memory for the div 
+    const grid = document.createElement('div');
+    //Append newly created grid element to the container div in HTML file 
+    container.append(grid);
+}
