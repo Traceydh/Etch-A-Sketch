8a273710c7a4278e5dd9008740dcfbcc4e080704
tracey <tracey.d.huang@gmail.com>
2022-06-14 14:01:47 +1000

    Define grid as global variable

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //create a webpage with 16x16 grid of square divs 
 //Select container element from HTML file 
 const container = document.querySelector('.container');
-//use for loop to repeat 16 times 
+let grid = document.createElement('div');
+//use for loop to repeat 16x16=256 times
 for (let i = 0; i < 256; i ++) {
-    //create column 
     //create element in memory for the div 
-    const grid = document.createElement('div');
+    grid = document.createElement('div');
     //Append newly created grid element to the container div in HTML file 
-    container.append(grid;
-
+    container.append(grid);
 }
 
-//Grid changes color when mouse passes over it
