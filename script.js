fac888ffc4ba0144088c1ff01d151df0d333df8b
tracey <tracey.d.huang@gmail.com>
2022-06-14 14:13:06 +1000

    Test change background color function logic

--- script.js
+++ script.js
@@ -6,7 +6,23 @@ let grid = document.createElement('div');
 for (let i = 0; i < 256; i ++) {
     //create element in memory for the div 
     grid = document.createElement('div');
+    //add attribute 
+    grid.id = i + 1;
     //Append newly created grid element to the container div in HTML file 
     container.append(grid);
+
 }
 
+//change color of grid when mouse is hovering over 
+grid.addEventListener("mouseover", () => {
+    grid.style.backgroundColor = 'salmon';
+})
+
+//grid is only the last grid element 
+console.log(grid);
+
+//change color of background logic testing
+const button = document.querySelector(".btn"); 
+button.addEventListener("click", () => {
+    button.style.backgroundColor = 'salmon';
+})
