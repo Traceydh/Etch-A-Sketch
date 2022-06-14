c9db6a684d4ccfef244dd7fd7cd63311164bde92
tracey <tracey.d.huang@gmail.com>
2022-06-14 14:47:39 +1000

    Connect user input and for loop to generate size of grid

--- script.js
+++ script.js
@@ -2,8 +2,21 @@
 //Select container element from HTML file 
 const container = document.querySelector('.container');
 let grid = document.createElement('div');
-//use for loop to repeat 16x16=256 times
-for (let i = 0; i < 256; i ++) {
+
+
+
+//change color of grid when mouse is hovering over 
+function changeColor() {
+    this.style.backgroundColor = 'salmon';
+}
+//perform popup when button is clicked 
+const btn = document.querySelector('#btn');
+btn.onclick = popUp; 
+//popup function ask user for input 
+function popUp () {
+   let size = prompt("Enter size of grid: ", "16");
+   //use for loop to repeat 16x16=256 times
+   for (let i = 0; i < size; i ++) {
     //create element in memory for the div 
     grid = document.createElement('div');
     //add attribute 
@@ -13,19 +26,6 @@ for (let i = 0; i < 256; i ++) {
     //Append newly created grid element to the container div in HTML file 
     container.append(grid);
 }
-
-//change color of grid when mouse is hovering over 
-function changeColor() {
-    this.style.backgroundColor = 'salmon';
-}
-
-//perform popup when button is clicked 
-const btn = document.querySelector('#btn');
-btn.onclick = popUp; 
-//popup function ask user for input 
-function popUp () {
-   let size = prompt("Enter size of grid: ", "16");
-   console.log(size);
 }
 //userInput should replace i, to remake the grid 
 //remake new grid 
