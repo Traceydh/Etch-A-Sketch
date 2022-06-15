a7b77be370431abf480676e2df78f56cfa0e49d0
tracey <tracey.d.huang@gmail.com>
2022-06-15 12:30:14 +1000

    Move column and row generation to JS instead of CSS

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 //create a webpage with 16x16 grid of square divs 
 //Select container element from HTML file 
-const body = document.body;
 const container = document.querySelector('.container');
-let grid = document.createElement('div');
-let color = 'black';
-drawGrid(16);
+let grid = drawGrid(16);
 
 //perform popup when button is clicked 
-const btn = document.querySelector('.btn');
-btn.onclick = popUp; 
+const resizeButton = document.querySelector('.resize-btn');
+resizeButton.onclick = popUp; 
+const resetButton = document.querySelector('.reset-btn');
+resetButton.onlick = reset; 
 
+//FUNCTION reset color of grid 
+function reset () {
+    gridList.forEach(grid => grid.style.backgroundColor = 'black');
+}
 
 //FUNCTION pop up asking for user input to restart grid 
 function popUp () {
-    //reset grid 
+
     let size = prompt("Enter size of grid: ", "16");
-   drawGrid(size);
+    if (size >= 100) {
+        return;
+    }
+    drawGrid(size);
 }
 
 //FUNCTION change color of grid when mouse is hovering over 
@@ -23,16 +29,19 @@ function changeColor() {
     this.style.backgroundColor = 'salmon';
 }
 
-//FUNCTION draw grid  
+//FUNCTION draw and color grid 
 function drawGrid (size) {
     for (let i = 0; i < size*size; i ++) {
+        //Column and row sizing
+        container.style.gridTemplateColumns = "repeat(16, 1fr)";
+        container.style.gridTemplateRows = "repeat(16, 1fr)";
         //create element in memory for the div 
-        grid = document.createElement('div');
-        //add attribute 
-        grid.id = i + 1;
+        let grid = document.createElement('div');
+        grid.style.backgroundColor = "black";
         //when mouse hovers over a div, change it's background color 
         grid.addEventListener('mouseover', changeColor);
         //Append newly created grid element to the container div in HTML file 
         container.append(grid);
+        gridList = document.querySelectorAll('.grid')
 }
 }
