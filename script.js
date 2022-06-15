19895e2be273368250660b6dff9b74780b2c6d63
tracey <tracey.d.huang@gmail.com>
2022-06-15 12:32:33 +1000

    Make size variable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //create a webpage with 16x16 grid of square divs 
 //Select container element from HTML file 
 const container = document.querySelector('.container');
-let grid = drawGrid(16);
+let grid = drawGrid(4);
 
 //perform popup when button is clicked 
 const resizeButton = document.querySelector('.resize-btn');
@@ -33,15 +33,14 @@ function changeColor() {
 function drawGrid (size) {
     for (let i = 0; i < size*size; i ++) {
         //Column and row sizing
-        container.style.gridTemplateColumns = "repeat(16, 1fr)";
-        container.style.gridTemplateRows = "repeat(16, 1fr)";
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         //create element in memory for the div 
         let grid = document.createElement('div');
         grid.style.backgroundColor = "black";
-        //when mouse hovers over a div, change it's background color 
-        grid.addEventListener('mouseover', changeColor);
         //Append newly created grid element to the container div in HTML file 
         container.append(grid);
-        gridList = document.querySelectorAll('.grid')
+        //when mouse hovers over a div, change it's background color 
+        grid.addEventListener('mouseover', changeColor);
 }
 }
